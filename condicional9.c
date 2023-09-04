d02de4b2a1e54b30423952878a02e324b84a9d65
Rafaela <rafaela.mumbach@acad.ufsm.br>
2023-09-04 17:22:39 -0300

    condicional

---
+++ condicional9.c
@@ -0,0 +1,10 @@
+//C00500 - Faça um algoritmo que leia 3 notas de um aluno e escreva sua média harmônica. Dica: A média harmônica entre três valores N1, N2 e N3
+//é calculada pela expressão 3/(1/N1+1/N2+1/N3). O que acontece se alguma das notas for igual a 0? Que resultado o algoritmo deve emitir? 
+//algoritmo "C001_C00000500"
+// Autor : rmbuenos
+// Data : 04/09/2023
+#include <stdio.h>
+
+void main(){
+	
+}
